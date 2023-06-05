fdb3edc699023014e41427dfe28331cfe6d35c82
Scott <scottalanmartin@gmail.com>
2023-06-05 16:03:44 -0600

    updates

--- CopyNewToExt.bat
+++ CopyNewToExt.bat
@@ -1,1 +1,3 @@
+D:\RoboCopy\Robocopy D:\Downloads J:\Downloads /E /DCOPY:DAT /IS /TEE /LOG+:D:\Robocopy\CopyNewToExt.log
+
 
--- ExtDriveSync.bat
+++ ExtDriveSync.bat
@@ -1,1 +1,5 @@
 
+D:\RoboCopy\robocopy J:\Downloads H:\Downloads /E /MIR /IS /TEE /LOG+:D:\Robocopy\ExtDriveSync.log
+
+
+
